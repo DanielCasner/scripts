e137005245c84479e7cdc0267f85ff89e6bba68b
Daniel Casner <daniel@anki.com>
2017-06-30 09:22:21 -0700

    Slightly better file naming.

--- python/camarchive.py
+++ python/camarchive.py
@@ -136,7 +136,7 @@ class CamArchiver(object):
         "Encodes the images to the output"
         output = os.path.abspath(os.path.join(
             output_directory,
-            "{name}{0:04d}-{1:02d}-{2:02d}.mp4".format(*time.localtime(), name=self.dir)))
+            "{name}_{0:04d}-{1:02d}-{2:02d}.mp4".format(*time.localtime(), name=self.dir)))
         vprint(1, "Encoding images to video {}".format(output))
         ffmpeg_loglevel = -8 + 8*V # See man ffmpeg
         self.subprocess = subprocess.Popen(["ffmpeg",
